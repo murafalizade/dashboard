45077cd12245c9d084b91ee9769d14c1f7a09643
murafalizade <hmmuralizade29@gmail.com>
2022-07-08 09:10:38 +0400

    percentage circle

---
+++ src/components/pages/home/compos/Keyword.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Keyword.scss';
+import { ProgressCircle } from './ProgressCircle/ProgressCircle';
+
+export const Keyword = () => {
+  return (
+    <div className='keyword'>
+        <h4>Açar Sözlər</h4>
+        <ProgressCircle />
+    </div>
+  )
+}
